d3a4539aaee1b0fb331b6832e11abfb651a42662
Jon Baird <jbaird29@gmail.com>
2021-03-03 20:49:53 -0500

    Add project files.

---
+++ Proj6_bairdjo.asm
@@ -0,0 +1,31 @@
+TITLE String Primitives and Macros     (Proj6_bairdjo.asm)
+
+; Author: Jon Baird
+; Last Modified: 3/4/2021
+; OSU email address: bairdjo@oregonstate.edu
+; Course number/section:   CS271 Section 401
+; Project Number: 6                Due Date: 3/14/2021
+; Description: This program reads 10 signed integers from the user via console input
+;              then calculates the sum and average and displays to the user.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+
+
+.code
+main PROC
+
+
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
